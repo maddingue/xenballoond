1756bc63a9901c8ba91907044c63731e5807cc67
Sebastien Aperghis-Tramoni <sebastien@aperghis.net>
2011-03-25 17:39:56 +0100

    add a meta-information module.

---
+++ lib/xenballoond/meta.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+""" program meta-information """
+
+name    = "xenballoond"
+version = "0.9.0"
+license = "GPL"
+authors = "Nicolas Agius, Sebastien Aperghis-Tramoni, David Passante"
+url     = "https://github.com/maddingue/xenballoond"
+description = "Xen virtual machine ballooning daemon"
