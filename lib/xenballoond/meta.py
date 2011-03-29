d7250c6985ba479fd07dae495b5bfcff65230a1f
Sebastien Aperghis-Tramoni <sebastien@aperghis.net>
2011-03-29 11:43:00 +0200

    add classifiers

--- lib/xenballoond/meta.py
+++ lib/xenballoond/meta.py
@@ -7,3 +7,17 @@ license = "GPL"
 authors = "Nicolas Agius, Sebastien Aperghis-Tramoni, David Passante"
 url     = "https://github.com/maddingue/xenballoond"
 description = "Xen virtual machine ballooning daemon"
+
+long_description = """ """
+
+classifiers = [
+    "Environment :: Console",
+    "Environment :: No Input/Output (Daemon)",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python",
+    "Topic :: System :: Operating System Kernels :: Linux",
+    "Topic :: System :: Systems Administration",
+]
+
