04213e702c4afae35a62cbd7d3dceacec87cbab2
Sebastien Aperghis-Tramoni <sebastien@aperghis.net>
2011-04-11 18:02:34 +0200

    also check with pyflakes, if available.

--- test/test_load.py
+++ test/test_load.py
@@ -1,20 +1,41 @@
-import sys, unittest
+import imp, re, sys, unittest
 
 sys.path += ["lib"]
-modules=[
+modules = [
     "xenballoond.cmdline",
     "xenballoond.meta",
     "xenballoond.xenballoon",
 ]
 
+# see if pyflakes is available
+have_pyflakes = 0
+try:
+    from pyflakes.scripts.pyflakes import checkPath
+    have_pyflakes = 1
+except:
+    pass
+
+
 class CheckLoad(unittest.TestCase):
     def test_load(self):
         for module in modules:
+            loaded = 0
+
+            # try to compile the module
             try:
                 __import__(module)
-                self.assertTrue(1, "loading %s" % module)
+                loaded = 1
             except:
-                self.assertTrue(0, "loading %s" % module)
+                pass
+
+            self.assertTrue(loaded, "loading %s" % module)
+
+            # check the module with pyflakes, if available
+            if loaded and have_pyflakes:
+                path = re.sub('c$', '', sys.modules[module].__file__)
+                has_flakes = checkPath(path)
+                self.assertTrue(not has_flakes,
+                    "checking %s with pyflakes" % module)
 
 
 if __name__ == "__main__":
